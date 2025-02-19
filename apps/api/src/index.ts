3ce6498403bbf4752b9a06d579942d88ab1c94de
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-19 19:59:57 -0300

    feat: api source code base

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'ADMIN' })
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUsers)
