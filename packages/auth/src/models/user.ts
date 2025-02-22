7e6b892c9a4fd4045af8ea56713fd6efa9988166
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-22 11:00:53 -0300

    add: models

--- packages/auth/src/models/user.ts
+++ packages/auth/src/models/user.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
-import { roleSchema, type Role } from '../roles'
+import { roleSchema } from '../roles'
 
 export const userSchema = z.object({
+	id: z.string(),
 	role: roleSchema,
 })
 export type User = z.infer<typeof userSchema>
