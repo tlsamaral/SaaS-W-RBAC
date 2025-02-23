8c73a2501da5aba34ec73f872fc18d5aa3d99ccd
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-23 15:20:38 -0300

    feat: add token jwt plugin

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,7 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 
 import {
@@ -7,6 +10,7 @@ import {
 	serializerCompiler,
 	validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -14,9 +18,30 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'Next.js SaaS',
+			description: 'Full-stack SaaS app with multi-tenant & RBAC',
+			version: '1.0.0',
+		},
+		servers: [],
+	},
+	transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+	routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+	secret: 'super-secret',
+})
+
 app.register(fastifyCors)
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: 3333 }).then(() => {
 	console.log('🚀 HTTP server running')
