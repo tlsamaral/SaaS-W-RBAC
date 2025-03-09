00be09adac86397084c9c4afc3e28cd499c0b02e
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-09 17:06:32 -0300

    feat: create util for get an user permissions

---
+++ apps/api/src/utils/get-user-permissions.ts
@@ -0,0 +1,12 @@
+import { type Role, defineAbilityFor, userSchema } from '@saas/auth'
+
+export function getUserPermissions(userId: string, role: Role) {
+	const authUser = userSchema.parse({
+		id: userId,
+		role: role,
+	})
+
+	const ability = defineAbilityFor(authUser)
+
+	return ability
+}
