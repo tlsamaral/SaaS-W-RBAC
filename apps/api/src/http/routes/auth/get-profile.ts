652a6a402330c8cbafc71a548e12bffbe7f0f6ab
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-08 23:04:43 -0300

    feat: add bearer token

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -16,6 +16,7 @@ export async function getProfile(app: FastifyInstance) {
 				schema: {
 					tags: ['Auth'],
 					summary: 'Get Authenticated User Profile',
+					security: [{ bearerAuth: [] }],
 					response: {
 						200: z.object({
 							user: z.object({
