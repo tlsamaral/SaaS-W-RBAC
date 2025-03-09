ce959621f14793f2b5c9fb1c06865030be0514a5
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-08 23:04:18 -0300

    create type funcion

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
+import type { Member, Organization } from '@prisma/client'
 import 'fastify'
 
 declare module 'fastify' {
 	interface FastifyRequest {
 		getCurrentUserId(): Promise<string>
+		getUserMemberShip(slug: string): Promise<{
+			organization: Organization
+			membership: Member
+		}>
 	}
 }
