255a8745d2b1b0bbb611da9f13fa78b9f0b76c67
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-08 23:02:19 -0300

    feat: create route to managed orgs

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,45 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { auth } from '../middlewares/auth'
+
+export async function getMembership(app: FastifyInstance) {
+	app
+		.withTypeProvider<ZodTypeProvider>()
+		.register(auth)
+		.get(
+			'/organization/:slug/membership',
+			{
+				schema: {
+					tags: ['Organizations'],
+					summary: 'Get organization membership on organization',
+					security: [{ bearerAuth: [] }],
+					params: z.object({
+						slug: z.string(),
+					}),
+					response: {
+						200: z.object({
+							membership: z.object({
+								id: z.string().uuid(),
+								role: z.string(),
+								organizationId: z.string().uuid(),
+							}),
+						}),
+					},
+				},
+			},
+			async (request) => {
+				const { slug } = request.params
+
+				const { membership } = await request.getUserMemberShip(slug)
+
+				return {
+					membership: {
+						role: membership.role,
+						id: membership.id,
+						organizationId: membership.organizationId,
+					},
+				}
+			},
+		)
+}
