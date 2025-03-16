cd45c8e4851e77a0eb4a44e46d1650023acbcc9c
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-16 12:20:22 -0300

    feat: revoke an invite

---
+++ apps/api/src/http/routes/invites/revoke-invite.ts
@@ -0,0 +1,66 @@
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { auth } from '../../middlewares/auth'
+import { BadRequestError } from '../_erros/bad-request-error'
+
+export async function revokeInvite(app: FastifyInstance) {
+	app
+		.withTypeProvider<ZodTypeProvider>()
+		.register(auth)
+		.post(
+			'/organizations/:slug/invites/:inviteId',
+			{
+				schema: {
+					tags: ['Invites'],
+					summary: 'Revoke an invite',
+					security: [{ bearerAuth: [] }],
+					params: z.object({
+						slug: z.string(),
+						inviteId: z.string().uuid(),
+					}),
+					response: {
+						204: z.null(),
+					},
+				},
+			},
+			async (request, reply) => {
+				const userId = await request.getCurrentUserId()
+
+				const { slug, inviteId } = request.params
+
+				const { organization, membership } =
+					await request.getUserMemberShip(slug)
+
+				const { cannot } = getUserPermissions(userId, membership.role)
+
+				if (cannot('delete', 'Invite')) {
+					throw new BadRequestError(
+						'You do not have permission to delete an invite',
+					)
+				}
+
+				const invite = await prisma.invite.findUnique({
+					where: {
+						id: inviteId,
+						organizationId: organization.id,
+					},
+				})
+
+				if (!invite) {
+					throw new BadRequestError('Invite not found')
+				}
+
+				await prisma.invite.delete({
+					where: {
+						id: inviteId,
+					},
+				})
+
+				return reply.status(204).send()
+			},
+		)
+}
