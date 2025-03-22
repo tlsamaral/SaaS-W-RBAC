5342045797e8795b581566af3605f8c7d5806ba8
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-21 21:20:11 -0300

    feat: verify if user is authenticated

--- apps/web/src/app/auth/layout.tsx
+++ apps/web/src/app/auth/layout.tsx
@@ -1,11 +1,18 @@
-export default function RootLayout({
-	children,
+import { isAuthenticated } from '@/auth/auth'
+import { redirect } from 'next/navigation'
+
+export default async function RootLayout({
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode
 }>) {
-	return (
-		<div className="flex min-h-screen items-center justify-center flex-col px-4">
-			<div className="w-full max-w-xs">{children}</div>
-		</div>
-	)
+  if (await isAuthenticated()) {
+    redirect('/')
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center flex-col px-4">
+      <div className="w-full max-w-xs">{children}</div>
+    </div>
+  )
 }
