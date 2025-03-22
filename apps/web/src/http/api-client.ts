ae2b30856fa2bca617e66567e1afb314e2f87e76
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-21 21:19:08 -0300

    feat: add bearer token

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,27 @@
 import ky from 'ky'
+import { getCookie } from 'cookies-next'
+
+import type { CookiesFn } from 'cookies-next/lib/common/types'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+
+        if (typeof window === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+
+          cookieStore = () => serverCookies()
+
+          const token = await getCookie('token', { cookies: cookieStore })
+
+          if (token) {
+            request.headers.set('Authorization', `Bearer ${token}`)
+          }
+        }
+      },
+    ],
+  },
 })
