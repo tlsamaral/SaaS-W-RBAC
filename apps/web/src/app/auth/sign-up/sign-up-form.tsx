71cf2fa8fbd182422e2dd47596762179309e9f6c
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-23 22:10:43 -0300

    feat: create clinet form to sign up

---
+++ apps/web/src/app/auth/sign-up/sign-up-form.tsx
@@ -0,0 +1,117 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+import Link from 'next/link'
+
+import githubIcon from '@/assets/github-icon.svg'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useFormState } from '@/hooks/use-form-state'
+import { signUpAction } from './actions'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { AlertTriangle, Loader2 } from 'lucide-react'
+import { signInWithGithub } from '../actions'
+
+export function SignUpForm() {
+  const router = useRouter()
+
+  const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
+    signUpAction,
+    () => {
+      router.push('/auth/sign-in')
+    },
+  )
+
+  return (
+    <div className="space-y-4">
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign in failed!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
+        )}
+        <div className="space-y-1">
+          <Label htmlFor="email">Name</Label>
+          <Input name="name" id="name" />
+
+          {errors?.name && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.name}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="email">E-mail</Label>
+          <Input name="email" type="email" id="email" />
+
+          {errors?.email && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.email}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="password">Password</Label>
+          <Input name="password" type="password" id="password" />
+
+          {errors?.password && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.password}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="password_confirmation">Confirme your password</Label>
+          <Input
+            name="password_confirmation"
+            type="password"
+            id="password_confirmation"
+          />
+
+          {errors?.password_confirmation && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.password_confirmation}
+            </p>
+          )}
+        </div>
+
+        <Button className="w-full" type="submit" disabled={isPending}>
+          {isPending ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            'Create account'
+          )}
+        </Button>
+
+        <Button className="w-full" variant="link" size="sm" asChild>
+          <Link href="/auth/sign-in">Already registered? Sign in</Link>
+        </Button>
+      </form>
+
+      <Separator />
+
+      <form action={signInWithGithub}>
+        <Button variant="outline" type="submit" className="w-full">
+          <Image
+            src={githubIcon}
+            alt=""
+            className="mr-2 size-4 dark:invert"
+            width={16}
+            height={16}
+          />{' '}
+          Sign up with GitHub
+        </Button>
+      </form>
+    </div>
+  )
+}
