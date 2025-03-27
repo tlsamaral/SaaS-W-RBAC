fd776574a1c6eaf6185443320fd7cd93aaefc983
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-26 21:45:24 -0300

    feat: including the switcher organization

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,11 +1,15 @@
-import { Code } from 'lucide-react'
+import { Code, Slash } from 'lucide-react'
 import { ProfileButton } from './profile-button'
+import { OrganizationSwicther } from './organization-switcher'
 
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Code className="size-6" />
+        <Slash className="size-3 -rotate-24 text-border" />
+
+        <OrganizationSwicther />
       </div>
 
       <div className="flex items-center gap-4">
