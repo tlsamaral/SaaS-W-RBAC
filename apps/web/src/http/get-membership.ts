9b9abbcfe9cb7b99da8ede2fc25a5a79956cd15c
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-28 20:18:57 -0300

    feat: add function to connect with api and get membership data

---
+++ apps/web/src/http/get-membership.ts
@@ -0,0 +1,19 @@
+import { api } from './api-client'
+import type { Role } from '@saas/auth'
+
+interface GetMembershipResponse {
+  membership: {
+    id: string
+    role: Role
+    userId: string
+    organizationId: string
+  }
+}
+
+export async function getMembership(org: string) {
+  const result = await api
+    .get(`organizations/${org}/membership`)
+    .json<GetMembershipResponse>()
+
+  return result
+}
