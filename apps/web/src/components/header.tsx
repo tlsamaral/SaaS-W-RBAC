d0d0c0b1495989cc07f22a5b8490e7ee491c0941
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-28 20:21:27 -0300

    feat: add toggle theme

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,8 +1,17 @@
 import { Code, Slash } from 'lucide-react'
 import { ProfileButton } from './profile-button'
 import { OrganizationSwicther } from './organization-switcher'
+import { ability } from '@/auth/auth'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { Separator } from './ui/separator'
 
-export function Header() {
+export async function Header() {
+  const permissions = await ability()
+
+  if (!permissions) {
+    return null
+  }
+
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
@@ -10,9 +19,13 @@ export function Header() {
         <Slash className="size-3 -rotate-24 text-border" />
 
         <OrganizationSwicther />
+
+        {permissions.can('get', 'Project') && <p>Projects</p>}
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
