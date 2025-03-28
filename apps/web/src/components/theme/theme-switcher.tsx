6c7fe2b6fd810884da6fa5e019963d37450e1feb
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-28 20:19:55 -0300

    feat: add add theme switcher

---
+++ apps/web/src/components/theme/theme-switcher.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Moon, Sun } from 'lucide-react'
+import { Button } from '../ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { useTheme } from 'next-themes'
+
+export function ThemeSwitcher() {
+  const { setTheme, resolvedTheme } = useTheme()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Sun className="size-4 scale-100 dark:scale-0" />
+          <Moon className="size-4 scale-0 dark:scale-100" />
+
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
