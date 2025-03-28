5183fc19944389ac3d5e2285ee525728afac15f6
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-28 20:22:47 -0300

    feat: create more functions

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,15 +1,47 @@
-import { getProfile } from '@/http/get-profile'
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-export async function isAuthenticated() {
-  const appCookies = await cookies()
 
-  return appCookies.has('token')
+import { getMembership } from '@/http/get-membership'
+import { getProfile } from '@/http/get-profile'
+
+export async function isAuthenticated() {
+  return !!(await cookies()).get('token')?.value
+}
+
+export async function getCurrentOrg() {
+  return (await cookies()).get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
-  const appCookies = await cookies()
-  const token = appCookies.get('token')?.value
+  const token = (await cookies()).get('token')?.value
 
   if (!token) {
     redirect('/auth/sign-in')
