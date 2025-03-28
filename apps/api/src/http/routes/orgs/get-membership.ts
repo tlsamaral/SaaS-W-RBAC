5f7083a7bda14e16bc16e1f5c92d78080ac2e718
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-28 20:20:33 -0300

    feat: include user id in response

--- apps/api/src/http/routes/orgs/get-membership.ts
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -5,42 +5,44 @@ import { z } from 'zod'
 import { auth } from '../../middlewares/auth'
 
 export async function getMembership(app: FastifyInstance) {
-	app
-		.withTypeProvider<ZodTypeProvider>()
-		.register(auth)
-		.get(
-			'/organizations/:slug/membership',
-			{
-				schema: {
-					tags: ['Organizations'],
-					summary: 'Get organization membership on organization',
-					security: [{ bearerAuth: [] }],
-					params: z.object({
-						slug: z.string(),
-					}),
-					response: {
-						200: z.object({
-							membership: z.object({
-								id: z.string().uuid(),
-								role: roleSchema,
-								organizationId: z.string().uuid(),
-							}),
-						}),
-					},
-				},
-			},
-			async (request) => {
-				const { slug } = request.params
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Get organization membership on organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.string().uuid(),
+                role: roleSchema,
+                userId: z.string().uuid(),
+                organizationId: z.string().uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
 
-				const { membership } = await request.getUserMemberShip(slug)
+        const { membership } = await request.getUserMemberShip(slug)
 
-				return {
-					membership: {
-						role: membership.role,
-						id: membership.id,
-						organizationId: membership.organizationId,
-					},
-				}
-			},
-		)
+        return {
+          membership: {
+            role: membership.role,
+            id: membership.id,
+            userId: membership.userId,
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
 }
