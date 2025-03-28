5b180cd54de242ab35dd34be224d0e40270e9aa1
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-28 20:23:22 -0300

    add env variable

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -2,9 +2,10 @@ import ky from 'ky'
 import { getCookie } from 'cookies-next'
 
 import type { CookiesFn } from 'cookies-next/lib/common/types'
+import { env } from '@saas/env'
 
 export const api = ky.create({
-  prefixUrl: 'http://localhost:3333',
+  prefixUrl: env.NEXT_PUBLIC_API_URL,
   hooks: {
     beforeRequest: [
       async (request) => {
