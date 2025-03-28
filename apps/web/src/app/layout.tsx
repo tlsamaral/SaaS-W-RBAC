b29bd7cb6695b9374c10e5350778b5e3a04611ba
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-28 20:22:29 -0300

    feat: add theme provider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,18 +1,27 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { ThemeProvider } from 'next-themes'
 
 export const metadata: Metadata = {
-	title: 'Create Next App',
+  title: 'Create Next App',
 }
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang="en" className="dark">
-			<body>{children}</body>
-		</html>
-	)
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
 }
