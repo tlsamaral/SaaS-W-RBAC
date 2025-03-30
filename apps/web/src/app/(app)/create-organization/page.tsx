3eff388d4635c3ec48ff1679416f2b2a039cf0f9
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-29 23:37:18 -0300

    add page to create an organization

---
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateOrganizationPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold"> Create organization</h1>
+    </div>
+  )
+}
