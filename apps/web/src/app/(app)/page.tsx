15861ef7db526592571fcd97bef3b478bc74ecb5
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-29 23:38:42 -0300

    index page

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,12 +1,3 @@
-import { auth } from '@/auth/auth'
-import { Header } from '@/components/header'
-
 export default async function Home() {
-  const { user } = await auth()
-
-  return (
-    <div className="py-4">
-      <Header />
-    </div>
-  )
+  return <h1>Projects</h1>
 }
