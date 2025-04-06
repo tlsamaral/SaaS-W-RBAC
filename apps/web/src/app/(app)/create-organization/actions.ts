f983fb139d1bb4de38071f432b6a4da5d1001bb2
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-05 23:30:27 -0300

    feat: action to create org

--- apps/web/src/app/(app)/create-organization/actions.ts
+++ apps/web/src/app/(app)/create-organization/actions.ts
@@ -2,26 +2,35 @@
 
 import { HTTPError } from 'ky'
 import { z } from 'zod'
-// import { createOrganization } from '@/http/sign-up'
+import { createOrganization } from '@/http/create-organization'
 
-const createOrganizationSchema = z
-  .object({
-    name: z.string().refine((value) => value.split(' ').length > 1, {
-      message: 'Please, enter your full name',
-    }),
-    email: z.string().email({ message: 'Please, provide a valid email' }),
-    password: z
-      .string()
-      .min(6, { message: 'Password must be at least 6 characters' }),
-    password_confirmation: z.string(),
-  })
-  .refine((data) => data.password === data.password_confirmation, {
-    message: 'Passwords do not match',
-    path: ['password_confirmation'],
-  })
+const organizationSchema = z.object({
+  name: z.string().min(4, { message: 'Please inlude at least 4 characters' }),
+  domain: z
+    .string()
+    .nullable()
+    .refine(
+      (value) => {
+        if (value) {
+          const domainRegex = /^(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/
+
+          return domainRegex.test(value)
+        }
+
+        return true
+      },
+      {
+        message: 'Please, provide a valid domain',
+      },
+    ),
+  shouldAttachUsersByDomain: z
+    .union([z.literal('on'), z.literal('off'), z.boolean()])
+    .transform((value) => value === 'on' || value === true)
+    .default(false),
+})
 
 export async function createOrganizationAction(data: FormData) {
-  const result = createOrganizationSchema.safeParse(Object.fromEntries(data))
+  const result = organizationSchema.safeParse(Object.fromEntries(data))
 
   if (!result.success) {
     const errors = result.error.flatten().fieldErrors
@@ -32,13 +41,13 @@ export async function createOrganizationAction(data: FormData) {
     }
   }
 
-  const { email, password, name } = result.data
+  const { name, domain, shouldAttachUsersByDomain } = result.data
 
   try {
     await createOrganization({
       name,
-      email,
-      password,
+      domain,
+      shouldAttachUsersByDomain,
     })
   } catch (err) {
     if (err instanceof HTTPError) {
