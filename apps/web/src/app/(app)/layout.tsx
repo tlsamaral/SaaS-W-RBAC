f575d02071e1750efdbdff8a276592ecd1ee187f
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-06 23:22:30 -0300

    remove header of base layout

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,20 +1,21 @@
 import { isAuthenticated } from '@/auth/auth'
-import { Header } from '@/components/header'
 import { redirect } from 'next/navigation'
 
 export default async function AppLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode
+  sheet: React.ReactNode
 }>) {
   if (!(await isAuthenticated())) {
     redirect('/auth/sign-in')
   }
 
   return (
-    <div className="py-4 space-y-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
-    </div>
+    <>
+      {children}
+      {sheet}
+    </>
   )
 }
