d69651ee8a830b24e38e7a09a6bcdac6320e0b54
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-15 22:41:03 -0300

    pre handler

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,7 +1,8 @@
-import { env } from '@saas/env'
-import { getCookie } from 'cookies-next'
-import type { CookiesFn } from 'cookies-next/lib/common/types'
 import ky from 'ky'
+import { getCookie } from 'cookies-next'
+
+import type { CookiesFn } from 'cookies-next/lib/common/types'
+import { env } from '@saas/env'
 
 export const api = ky.create({
   prefixUrl: env.NEXT_PUBLIC_API_URL,
@@ -13,12 +14,13 @@ export const api = ky.create({
         if (typeof window === 'undefined') {
           const { cookies: serverCookies } = await import('next/headers')
 
-          cookieStore = serverCookies
-        }
-        const token = getCookie('token', { cookies: cookieStore })
+          cookieStore = () => serverCookies()
 
-        if (token) {
-          request.headers.set('Authorization', `Bearer ${token}`)
+          const token = await getCookie('token', { cookies: cookieStore })
+
+          if (token) {
+            request.headers.set('Authorization', `Bearer ${token}`)
+          }
         }
       },
     ],
