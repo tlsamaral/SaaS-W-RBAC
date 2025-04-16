c523a205ad0fe98c6e042abe55dacb0d2a073e0d
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-04-15 22:27:42 -0300

    valid auth token

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,8 +1,7 @@
-import ky from 'ky'
-import { getCookie } from 'cookies-next'
-
-import type { CookiesFn } from 'cookies-next/lib/common/types'
 import { env } from '@saas/env'
+import { getCookie } from 'cookies-next'
+import type { CookiesFn } from 'cookies-next/lib/common/types'
+import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: env.NEXT_PUBLIC_API_URL,
@@ -14,13 +13,12 @@ export const api = ky.create({
         if (typeof window === 'undefined') {
           const { cookies: serverCookies } = await import('next/headers')
 
-          cookieStore = () => serverCookies()
+          cookieStore = serverCookies
+        }
+        const token = getCookie('token', { cookies: cookieStore })
 
-          const token = await getCookie('token', { cookies: cookieStore })
-
-          if (token) {
-            request.headers.set('Authorization', `Bearer ${token}`)
-          }
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
         }
       },
     ],
