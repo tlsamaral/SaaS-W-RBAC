c3757956c199924d4a75641a1f69f6ab6ba4891f
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-07-01 19:59:16 -0300

    interceptor route to create a project

---
+++ apps/web/src/app/(app)/@sheet/(.)org/[slug]/create-project/page.tsx
@@ -0,0 +1,20 @@
+import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+
+import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
+import { ProjectForm } from '@/app/(app)/org/[slug]/create-project/project-form'
+
+export default function CreateProject() {
+  return (
+    <Sheet defaultOpen>
+      <InterceptedSheetContent>
+        <SheetHeader>
+          <SheetTitle>Create Project</SheetTitle>
+        </SheetHeader>
+
+        <div className="p-4">
+          <ProjectForm />
+        </div>
+      </InterceptedSheetContent>
+    </Sheet>
+  )
+}
