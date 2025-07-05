a7d0b0acb411a7a06e9b74ed4b5194a46a2753c5
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-07-05 11:53:32 -0300

    feat: create action to udpate a organization

--- apps/web/src/app/(app)/org/actions.ts
+++ apps/web/src/app/(app)/org/actions.ts
@@ -4,8 +4,10 @@ import { HTTPError } from 'ky'
 import { z } from 'zod'
 
 import { createOrganization } from '@/http/create-organization'
+import { getCurrentOrg } from '@/auth/auth'
+import { updateOrganization } from '@/http/update-organization'
 
-export const organizationSchema = z
+const organizationSchema = z
   .object({
     name: z
       .string()
@@ -61,6 +63,49 @@ export async function createOrganizationAction(data: FormData) {
 
   try {
     await createOrganization({
+      name,
+      domain,
+      shouldAttachUsersByDomain,
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the organization.',
+    errors: null,
+  }
+}
+
+export async function updateOrganizationAction(data: FormData) {
+  const currentOrg = await getCurrentOrg()
+
+  const result = organizationSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { name, domain, shouldAttachUsersByDomain } = result.data
+
+  try {
+    await updateOrganization({
+      org: currentOrg,
       name,
       domain,
       shouldAttachUsersByDomain,
