58057f59b1a5834b04ae5fb72c67193f266b61b8
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-07-07 20:12:22 -0300

    feat: create route to get members

---
+++ apps/web/src/http/get-members.ts
@@ -0,0 +1,20 @@
+import type { Role } from '@saas/auth'
+import { api } from './api-client'
+
+interface GetMembersResponse {
+  members: {
+    name: string | null
+    id: string
+    avatarUrl: string | null
+    role: Role
+    email: string
+  }[]
+}
+
+export async function getMembers(org: string) {
+  const result = await api
+    .get(`organizations/${org}/members`)
+    .json<GetMembersResponse>()
+
+  return result
+}
