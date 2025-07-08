5cc02af1788e0b2a22e7d9f80ce1876236634d75
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-07-07 21:52:36 -0300

    feat: update returned data

--- apps/api/src/http/routes/members/get-members.ts
+++ apps/api/src/http/routes/members/get-members.ts
@@ -3,87 +3,87 @@ import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
 import { auth } from '@/http/middlewares/auth'
-import { BadRequestError } from '@/http/routes/_erros/bad-request-error'
 import { UnauthorizedError } from '@/http/routes/_erros/unauthorized-error'
 import { prisma } from '@/lib/prisma'
 import { getUserPermissions } from '@/utils/get-user-permissions'
 import { roleSchema } from '@saas/auth'
 
 export async function getMembers(app: FastifyInstance) {
-	app
-		.withTypeProvider<ZodTypeProvider>()
-		.register(auth)
-		.get(
-			'/organizations/:slug/members',
-			{
-				schema: {
-					tags: ['Members'],
-					summary: 'Get all organization members',
-					security: [{ bearerAuth: [] }],
-					params: z.object({
-						slug: z.string(),
-					}),
-					response: {
-						200: z.object({
-							members: z.array(
-								z.object({
-									name: z.string().nullable(),
-									avatarUrl: z.string().url().nullable(),
-									email: z.string(),
-									id: z.string(),
-									role: roleSchema,
-								}),
-							),
-						}),
-					},
-				},
-			},
-			async (request, reply) => {
-				const { slug } = request.params
-				const userId = await request.getCurrentUserId()
-				const { organization, membership } =
-					await request.getUserMemberShip(slug)
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/members',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Get all organization members',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              members: z.array(
+                z.object({
+                  name: z.string().nullable(),
+                  avatarUrl: z.string().url().nullable(),
+                  email: z.string(),
+                  id: z.string(),
+                  role: roleSchema,
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMemberShip(slug)
 
-				const { cannot } = getUserPermissions(userId, membership.role)
+        const { cannot } = getUserPermissions(userId, membership.role)
 
-				if (cannot('get', 'User')) {
-					throw new UnauthorizedError(
-						`You're not allowed to see organization members.`,
-					)
-				}
+        if (cannot('get', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to see organization members.`,
+          )
+        }
 
-				const members = await prisma.member.findMany({
-					select: {
-						id: true,
-						role: true,
-						user: {
-							select: {
-								id: true,
-								name: true,
-								email: true,
-								avatarUrl: true,
-							},
-						},
-					},
-					where: {
-						organizationId: organization.id,
-					},
-					orderBy: {
-						role: 'asc',
-					},
-				})
+        const members = await prisma.member.findMany({
+          select: {
+            id: true,
+            role: true,
+            user: {
+              select: {
+                id: true,
+                name: true,
+                email: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            organizationId: organization.id,
+          },
+          orderBy: {
+            role: 'asc',
+          },
+        })
 
-				const membersWithRoles = members.map(
-					({ id, role, user: { id: userId, ...user } }) => {
-						return {
-							id,
-							role,
-							...user,
-						}
-					},
-				)
+        const membersWithRoles = members.map(
+          ({ id, role, user: { id: userId, ...user } }) => {
+            return {
+              id,
+              role,
+              userId,
+              ...user,
+            }
+          },
+        )
 
-				return reply.send({ members: membersWithRoles })
-			},
-		)
+        return reply.send({ members: membersWithRoles })
+      },
+    )
 }
