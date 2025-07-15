204afb16cfb9aec3e7a78cb9f5d81ba13fbbda72
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-07-15 20:41:27 -0300

    feat: create action to update a user

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -2,12 +2,22 @@
 
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { updateMember } from '@/http/update-member'
+import type { Role } from '@saas/auth'
 import { revalidateTag } from 'next/cache'
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg()
 
   await removeMember({ org: currentOrg, memberId })
+
+  revalidateTag(`${currentOrg}/members`)
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = await getCurrentOrg()
+
+  await updateMember({ org: currentOrg, memberId, role })
 
   revalidateTag(`${currentOrg}/members`)
 }
