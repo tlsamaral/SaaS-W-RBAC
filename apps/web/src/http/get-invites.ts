42019cd7d7ad51f9bf7af934e46bb21a4ec7753c
Talles <worktalles@hotmail.com>
2025-07-22 20:13:13 -0300

    feat: create invites page

---
+++ apps/web/src/http/get-invites.ts
@@ -0,0 +1,23 @@
+import type { Role } from '@saas/auth'
+import { api } from './api-client'
+
+interface GetInvitesResponse {
+  invites: {
+    id: string
+    createdAt: Date
+    role: Role
+    email: string
+    author: {
+      name: string | null
+      id: string
+    } | null
+  }[]
+}
+
+export async function getInvites(org: string) {
+  const result = await api
+    .get(`organizations/${org}/invites`)
+    .json<GetInvitesResponse>()
+
+  return result
+}
