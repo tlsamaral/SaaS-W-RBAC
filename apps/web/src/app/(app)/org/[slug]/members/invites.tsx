14e99a50daf629a92dce1cc304587c9490967e75
Talles <worktalles@hotmail.com>
2025-07-22 20:14:24 -0300

    feat: create invites page

--- apps/web/src/app/(app)/org/[slug]/members/invites.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/invites.tsx
@@ -1,3 +1,60 @@
-export function Invites() {
-  return <div>Invites</div>
+import { ability, getCurrentOrg } from '@/auth/auth'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import { getInvites } from '@/http/get-invites'
+import { XOctagon } from 'lucide-react'
+
+export async function Invites() {
+  const currentOrg = await getCurrentOrg()
+  const permissions = await ability()
+  const { invites } = await getInvites(currentOrg)
+
+  return (
+    <div className="space-y-4">
+      {permissions?.can('create', 'Invite') && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Invites</CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      )}
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">Invites</h2>
+
+        <div className="rounded border">
+          <Table>
+            <TableBody>
+              {invites.map((invite) => {
+                return (
+                  <TableRow key={invite.id}>
+                    <TableCell className="py-2.5">
+                      <span className="text-muted-foreground">
+                        {invite.email}
+                      </span>
+                    </TableCell>
+                    <TableCell className="py-2.5 font-medium">
+                      {invite.role}
+                    </TableCell>
+                    <TableCell className="py-2.5">
+                      <div className="flex justify-end">
+                        {permissions.can('delete', 'Invite') && (
+                          <Button variant="destructive">
+                            <XOctagon className="size-4" />
+                            Revoke Invite
+                          </Button>
+                        )}
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  )
 }
