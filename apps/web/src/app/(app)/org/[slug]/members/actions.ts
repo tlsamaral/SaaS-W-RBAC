33feb9247c963a734c767440d4286ce660fcb0f7
Talles <worktalles@hotmail.com>
2025-07-22 20:30:34 -0300

    feat: action to revoke a invite

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -2,6 +2,7 @@
 
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { revokeInvite } from '@/http/revoke-invite'
 import { updateMember } from '@/http/update-member'
 import type { Role } from '@saas/auth'
 import { revalidateTag } from 'next/cache'
@@ -20,4 +21,12 @@ export async function updateMemberAction(memberId: string, role: Role) {
   await updateMember({ org: currentOrg, memberId, role })
 
   revalidateTag(`${currentOrg}/members`)
+}
+
+export async function revokeInviteAction(inviteId: string) {
+  const currentOrg = await getCurrentOrg()
+
+  await revokeInvite({ org: currentOrg, inviteId })
+
+  revalidateTag(`${currentOrg}/invites`)
 }
