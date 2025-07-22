517ca487823cb37ed79f2b46d3a222d8a14f01d7
Talles <worktalles@hotmail.com>
2025-07-22 20:32:07 -0300

    chore: update method

--- apps/api/src/http/routes/invites/revoke-invite.ts
+++ apps/api/src/http/routes/invites/revoke-invite.ts
@@ -1,6 +1,5 @@
 import { prisma } from '@/lib/prisma'
 import { getUserPermissions } from '@/utils/get-user-permissions'
-import { roleSchema } from '@saas/auth'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
@@ -8,59 +7,59 @@ import { auth } from '../../middlewares/auth'
 import { BadRequestError } from '../_erros/bad-request-error'
 
 export async function revokeInvite(app: FastifyInstance) {
-	app
-		.withTypeProvider<ZodTypeProvider>()
-		.register(auth)
-		.post(
-			'/organizations/:slug/invites/:inviteId',
-			{
-				schema: {
-					tags: ['Invites'],
-					summary: 'Revoke an invite',
-					security: [{ bearerAuth: [] }],
-					params: z.object({
-						slug: z.string(),
-						inviteId: z.string().uuid(),
-					}),
-					response: {
-						204: z.null(),
-					},
-				},
-			},
-			async (request, reply) => {
-				const userId = await request.getCurrentUserId()
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/invites/:inviteId',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Revoke an invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
 
-				const { slug, inviteId } = request.params
+        const { slug, inviteId } = request.params
 
-				const { organization, membership } =
-					await request.getUserMemberShip(slug)
+        const { organization, membership } =
+          await request.getUserMemberShip(slug)
 
-				const { cannot } = getUserPermissions(userId, membership.role)
+        const { cannot } = getUserPermissions(userId, membership.role)
 
-				if (cannot('delete', 'Invite')) {
-					throw new BadRequestError(
-						'You do not have permission to delete an invite',
-					)
-				}
+        if (cannot('delete', 'Invite')) {
+          throw new BadRequestError(
+            'You do not have permission to delete an invite',
+          )
+        }
 
-				const invite = await prisma.invite.findUnique({
-					where: {
-						id: inviteId,
-						organizationId: organization.id,
-					},
-				})
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+            organizationId: organization.id,
+          },
+        })
 
-				if (!invite) {
-					throw new BadRequestError('Invite not found')
-				}
+        if (!invite) {
+          throw new BadRequestError('Invite not found')
+        }
 
-				await prisma.invite.delete({
-					where: {
-						id: inviteId,
-					},
-				})
+        await prisma.invite.delete({
+          where: {
+            id: inviteId,
+          },
+        })
 
-				return reply.status(204).send()
-			},
-		)
+        return reply.status(204).send()
+      },
+    )
 }
