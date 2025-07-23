bef109cae6911dc45ed764b2e4c140fbb1f4036b
Talles <worktalles@hotmail.com>
2025-07-22 21:41:02 -0300

    chore: create action to register a new invite

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,11 +1,63 @@
 'use server'
 
 import { getCurrentOrg } from '@/auth/auth'
+import { createInvite } from '@/http/create-invite'
 import { removeMember } from '@/http/remove-member'
 import { revokeInvite } from '@/http/revoke-invite'
 import { updateMember } from '@/http/update-member'
-import type { Role } from '@saas/auth'
+import { roleSchema, type Role } from '@saas/auth'
+import { HTTPError } from 'ky'
 import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
+
+const inviteSchema = z.object({
+  email: z.string().email({ message: 'Invalid email' }),
+  role: roleSchema,
+})
+
+export async function createInviteAction(data: FormData) {
+  const result = inviteSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, role } = result.data
+
+  const org = await getCurrentOrg()
+
+  try {
+    await createInvite({
+      org,
+      email,
+      role,
+    })
+
+    revalidateTag(`${org}/invites`)
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the project.',
+    errors: null,
+  }
+}
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg()
