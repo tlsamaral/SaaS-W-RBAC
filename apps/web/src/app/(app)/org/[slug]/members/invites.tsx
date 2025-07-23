49f817d90074b2364591c76b24458ceb3a4932a9
Talles <worktalles@hotmail.com>
2025-07-22 21:41:42 -0300

    chore: use form to invites

--- apps/web/src/app/(app)/org/[slug]/members/invites.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/invites.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
 import { getInvites } from '@/http/get-invites'
 import { RevokeInviteButton } from './revoke-invite-button'
+import { CreateInviteForm } from './create-invite-form'
 
 export async function Invites() {
   const currentOrg = await getCurrentOrg()
@@ -16,7 +17,9 @@ export async function Invites() {
           <CardHeader>
             <CardTitle>Invites</CardTitle>
           </CardHeader>
-          <CardContent></CardContent>
+          <CardContent>
+            <CreateInviteForm />
+          </CardContent>
         </Card>
       )}
 
