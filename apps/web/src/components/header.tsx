5f992931a80b46ab31f3193e1622af6f572286bc
Talles <worktalles@hotmail.com>
2025-07-26 11:34:54 -0300

    chore: use pending invites content

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -5,6 +5,7 @@ import { ability } from '@/auth/auth'
 import { ThemeSwitcher } from './theme/theme-switcher'
 import { Separator } from './ui/separator'
 import { ProjectSwitcher } from './project-switcher'
+import { PendingInvites } from './pending-invites'
 
 export async function Header() {
   const permissions = await ability()
@@ -26,6 +27,7 @@ export async function Header() {
       </div>
 
       <div className="flex items-center gap-4">
+        <PendingInvites />
         <ThemeSwitcher />
         <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
